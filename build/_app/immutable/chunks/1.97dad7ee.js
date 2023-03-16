288ff67c527443abb8902ed88c60fccf52cc183c
Elias <elias.sabja@uc.cl>
2023-03-16 11:25:20 +0000

    Added build

---
+++ build/_app/immutable/chunks/1.97dad7ee.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.55378dd5.js";export{t as component};
---
+++ build/_app/immutable/chunks/2.47974703.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.90dfec92.js";export{t as component};
