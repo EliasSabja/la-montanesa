ea0ff46263bf3d1daec68fe8dfc2a7a64aeb6f28
Elias <elias.sabja@uc.cl>
2023-03-18 10:54:28 +0000

    Changed build directory

--- build/_app/immutable/chunks/1.97dad7ee.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.55378dd5.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1.05538468.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.4ca07de1.js";export{t as component};
